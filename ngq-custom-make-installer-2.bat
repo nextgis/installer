84a13cc58af6af50db93140e02b058c431587540
alisovenko <alexander.lisovenko@gmail.com>
2015-06-24 22:43:49 +0300

    NGQ make installer script use customization dir

---
+++ ngq-custom-make-installer-2.bat
@@ -0,0 +1,12 @@
+@echo off
+rem %1 - customization dir
+rem %2 - qgis build install
+rem %3 - installer dest dir
+
+call %CONFIGURE_ENV_WIN32%
+
+echo "======== Make ngq installer Start==========="
+
+%OSGEO4W_ROOT%\bin\python.exe make-installer.py --customization_dir %1 --build_num 0 --qgis_output %2 --installer_dir_dst %3
+
+echo "======== Make ngq installer Finish==========="
